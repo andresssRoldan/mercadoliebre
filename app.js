e96bc0888b96a41c38c31883bb2c6b99bbfb2798
andresssroldan <roldan_17@hotmail.com>
2022-05-02 23:04:01 -0500

    nueva configuracion heroku

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
-app.listen(procces.env.PORT || 3030, () => {
+app.listen(port, () => {
   console.log("Levantando el Servidor");
 });
 
@@ -12,9 +13,9 @@ app.listen(procces.env.PORT || 3030, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
-app.get('/register.html', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/register.html"))
 })
-app.get('/login.html', (req, res) =>{
+app.get('/login', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
